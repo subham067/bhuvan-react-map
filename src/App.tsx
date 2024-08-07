aeef20ba1f0ab4b45d01f9df153d10ee526f79a6
Subham Ghosh <subhamghosh@Subhams-MacBook-Air.local>
2024-08-07 12:21:18 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import MapComponent from './MapComponent'; // Adjust the path as needed
 
 function App() {
